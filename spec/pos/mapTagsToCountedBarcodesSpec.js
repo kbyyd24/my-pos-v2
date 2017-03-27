ec9d122297526f8c53de6e98046f2c2133cdcdea
gaoyx <melo@gaoyuexiang.cn>
2017-03-28 00:17:48 +0800

    add test case for given many tags

--- spec/pos/mapTagsToCountedBarcodesSpec.js
+++ spec/pos/mapTagsToCountedBarcodesSpec.js
@@ -8,9 +8,17 @@ describe('map tags to barcodes with count', () => {
     const expectCountedBarcode = [{barcode: 'ITEM00001', count: 1}];
     expect(mapTagsToCountedBarcodes(tags)).toEqual(expectCountedBarcode);
   });
-  it('should return countedBarcodes when given tag with delimiter', function () {
+  it('should return countedBarcodes when given tag with delimiter', () => {
     const tags = ['ITEM00001-4'];
     const expectCountedBarcode = [{barcode: 'ITEM00001', count: 4}];
     expect(mapTagsToCountedBarcodes(tags)).toEqual(expectCountedBarcode);
+  });
+  it('should return countedBarcodes when given many tags', () => {
+    const tags = ['ITEM00001', 'ITEM00001-4', 'ITEM00003-2.5'];
+    const expectCountedBarcodes = [
+      {barcode: 'ITEM00001', count: 5},
+      {barcode: 'ITEM00003', count: 2.5}
+    ];
+    expect(mapTagsToCountedBarcodes(tags)).toEqual(expectCountedBarcodes);
   });
 });
