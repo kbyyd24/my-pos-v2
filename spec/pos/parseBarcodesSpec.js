3e202a1cb36b4febbd731fbaf511c2093c2d78f8
gaoyx <melo@gaoyuexiang.cn>
2017-03-27 23:14:05 +0800

    [refactor[ use const to instead of let

--- spec/pos/parseBarcodesSpec.js
+++ spec/pos/parseBarcodesSpec.js
@@ -1,29 +1,29 @@
 describe('parse barcodes', () => {
-  let parseBarcodes = require('../../lib/pos/parseBarcodes');
+  const parseBarcodes = require('../../lib/pos/parseBarcodes');
   it('should return unContedBarcodes without split char when given barcodes without count', () => {
-    let barcodes = [
+    const barcodes = [
       'ITEM00001',
       'ITEM00003',
       'ITEM00005'
     ];
-    let expectResult = [
+    const expectResult = [
       {barcode: 'ITEM00001', count: 1},
       {barcode: 'ITEM00003', count: 1},
       {barcode: 'ITEM00005', count: 1}
     ];
-    let unCountedBarcodes = parseBarcodes(barcodes);
+    const unCountedBarcodes = parseBarcodes(barcodes);
     expect(unCountedBarcodes).toEqual(expectResult);
   });
   it('should return unCountedBarcodes with split char when given barcodes with count', () => {
-    let barcodes = [
+    const barcodes = [
       'ITEM00001-2',
       'ITEM00003-3.2'
     ];
-    let expectResult = [
+    const expectResult = [
       {barcode: 'ITEM00001', count: 2},
       {barcode: 'ITEM00003', count: 3.2}
     ];
-    let unCountedBarcodes = parseBarcodes(barcodes);
+    const unCountedBarcodes = parseBarcodes(barcodes);
     expect(unCountedBarcodes).toEqual(expectResult);
   })
 });
