ea45604d432c7a92bff5ea9f1e9ecc4a57f6f0a3
gaoyx <melo@gaoyuexiang.cn>
2017-03-27 11:37:12 +0800

    add parseBarcodesSpec

---
+++ spec/pos/parseBarcodesSpec.js
@@ -0,0 +1,28 @@
+describe('parse barcodes', () => {
+  it('should return unContedBarcodes with count as 1 when given barcodes without count', () => {
+    let barcodes = [
+      'ITEM000001',
+      'ITEM000003',
+      'ITEM000005'
+    ];
+    let expectResult = [
+      {barcode: 'ITEM00001', count: 1},
+      {barcode: 'ITEM00003', count: 1},
+      {barcode: 'ITEM00005', count: 1}
+    ]
+    let unCountedBarcodes = parseBarcodes(barcodes);
+    expect(unCountedBarcodes).toEqual(expectResult);
+  });
+  it('should return unCountedBarcodes when given barcodes with count', () => {
+    let barcodes = [
+      'ITEM00001-2',
+      'ITEM00003-3.2'
+    ];
+    let expectResult = [
+      {barcode: 'ITEM00001', count: 2},
+      {barcode: 'ITEM00003', count: 3.2}
+    ];
+    let unCountedBarcodes = parseBarcodes(barcodes);
+    expect(unCountedBarcodes).toEqual(expectResult);
+  })
+});
