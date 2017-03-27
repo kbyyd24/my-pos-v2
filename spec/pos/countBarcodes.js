45be9fa957959e45500af2b1c7d2df7ba500caac
gaoyx <melo@gaoyuexiang>
2017-03-27 15:37:38 +0800

    add test cases for countBarcodes

---
+++ spec/pos/countBarcodes.js
@@ -0,0 +1,37 @@
+describe('count barcodes', () => {
+  it('should return countedBarcodes when given unCountedBarcodes without repeat barcode', () => {
+    const noRepeatBarcode = [
+      {barcode: 'ITEM00001', count: 1},
+      {barcode: 'ITEM00003', count: 1},
+      {barcode: 'ITEM00005', count: 1}
+    ];
+    const expectCountedBarcodes = [
+      {barcode: 'ITEM00001', count: 1},
+      {barcode: 'ITEM00003', count: 1},
+      {barcode: 'ITEM00005', count: 1}
+    ];
+    const countedBarcodes = countBarcodes(noRepeatBarcode);
+    for (let i = 0; i < countedBarcodes.length; i++) {
+      expect(countedBarcodes[i].barcode).toEqual(expectCountedBarcodes[i].barcode);
+      expect(countedBarcodes[i].count).toEqual(expectCountedBarcodes[i].count);
+    }
+  });
+  it('should return countedBarcodes when given unCountedBarcodes with repeat barcodes', () => {
+    const repeatBarcode = [
+      {barcode: 'ITEM00001', count: 1},
+      {barcode: 'ITEM00001', count: 4.5},
+      {barcode: 'ITEM00003', count: 1},
+      {barcode: 'ITEM00005', count: 1}
+    ];
+    const expectCountedBarcodes = [
+      {barcode: 'ITEM00001', count: 5.5},
+      {barcode: 'ITEM00003', count: 1},
+      {barcode: 'ITEM00005', count: 1}
+    ];
+    const countedBarcodes = countBarcodes(repeatBarcode);
+    for (let i = 0; i < countedBarcodes.length; i++) {
+      expect(countedBarcodes[i].barcode).toEqual(expectCountedBarcodes[i].barcode);
+      expect(countedBarcodes[i].count).toEqual(expectCountedBarcodes[i].count);
+    }
+  })
+});
