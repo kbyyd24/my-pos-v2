90f3d666fc348a3faac6459651e2c9b7a49fb3da
gaoyx <melo@gaoyuexiang.cn>
2017-03-27 20:43:14 +0800

    fixed test cases for computeEachPrice

--- spec/pos/computeEachPriceSpec.js
+++ spec/pos/computeEachPriceSpec.js
@@ -1,4 +1,5 @@
 describe('compute each item price', () => {
+  let computeEachPrice = require('../../lib/pos/computeEachPrice');
   it('should return origin price when given a item without promotion', () => {
     const cartItem = {name: '荔枝', price: 15.00, unit: '斤', count: 1, promotion: ''};
     const expectOrderItem = {name: '荔枝', price: 15.00, unit: '斤', count: 1, sumPrice: 15.00, savePrice: 0};
@@ -9,7 +10,7 @@ describe('compute each item price', () => {
   it('should return origin price when given a item with promotion but not enable', () => {
     const cartItem = {name: '方便面', price: 4.50, unit: '袋', count: 1, promotion: 'BUY_TWO_GET_ONE_FREE'};
     const expectOrderItem = {name: '方便面', price: 4.50, unit: '袋', count: 1, sumPrice: 4.50, savePrice: 0};
-    const orderItem = computEachPrice(cartItem);
+    const orderItem = computeEachPrice(cartItem);
     expect(orderItem.sumPrice).toEqual(expectOrderItem.sumPrice);
     expect(orderItem.savePrice).toEqual(expectOrderItem.savePrice);
   });
