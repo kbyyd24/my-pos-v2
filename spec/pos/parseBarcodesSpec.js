18ff3ceb2f825679a7181ae4555bc22ec96ab8d9
gaoyx <melo@gaoyuexiang>
2017-03-27 11:47:31 +0800

    fixed test case 1

--- spec/pos/parseBarcodesSpec.js
+++ spec/pos/parseBarcodesSpec.js
@@ -1,9 +1,10 @@
 describe('parse barcodes', () => {
+  let parseBarcodes = require('../../lib/pos/parseBarcodes');
   it('should return unContedBarcodes without split char when given barcodes without count', () => {
     let barcodes = [
-      'ITEM000001',
-      'ITEM000003',
-      'ITEM000005'
+      'ITEM00001',
+      'ITEM00003',
+      'ITEM00005'
     ];
     let expectResult = [
       {barcode: 'ITEM00001', count: 1},
@@ -11,7 +12,10 @@ describe('parse barcodes', () => {
       {barcode: 'ITEM00005', count: 1}
     ]
     let unCountedBarcodes = parseBarcodes(barcodes);
-    expect(unCountedBarcodes).toEqual(expectResult);
+    for (let i = 0; i < unCountedBarcodes.length; i++) {
+      expect(unCountedBarcodes[i].barcode).toEqual(expectResult[i].barcode);
+      expect(unCountedBarcodes[i].count).toEqual(expectResult[i].count);
+    }
   });
   it('should return unCountedBarcodes with split char when given barcodes with count', () => {
     let barcodes = [
