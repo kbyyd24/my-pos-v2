6a4d4246691cef4fbe49a32a462482937bd963a4
gaoyx <melo@gaoyuexiang.cn>
2017-03-27 16:33:28 +0800

    udpate test case

--- spec/pos/getCartItemsWithPromotionSpec.js
+++ spec/pos/getCartItemsWithPromotionSpec.js
@@ -11,9 +11,9 @@ describe('get cart items this promotion info', () => {
       {name: '方便面', price: 4.50, unit: '袋', barcode: 'ITEM000005', count: 1}
     ];
     const expectCartItemsWithPromotion = [
-      {name: '雪碧', price: 3.00, unit: '瓶', barcode: 'ITEM000001', count: 5, promotion: 'BUY_TWO_GET_ONE_FREE'},
-      {name: '荔枝', price: 15.00, unit: '斤', barcode: 'ITEM000003', count: 1, promotion: ''},
-      {name: '方便面', price: 4.50, unit: '袋', barcode: 'ITEM000005', count: 1, promotion: 'BUY_TWO_GET_ONE_FREE'}
+      {name: '雪碧', price: 3.00, unit: '瓶', count: 5, promotion: 'BUY_TWO_GET_ONE_FREE'},
+      {name: '荔枝', price: 15.00, unit: '斤', count: 1, promotion: ''},
+      {name: '方便面', price: 4.50, unit: '袋', count: 1, promotion: 'BUY_TWO_GET_ONE_FREE'}
     ];
     const cartItemsWithPromotion = getCartItemsWithPromotion(cartItems);
     for (let i = 0; i < cartItemsWithPromotion.length; i++) {
