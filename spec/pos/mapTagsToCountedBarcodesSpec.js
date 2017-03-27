14884d0756049423ccf55bd903f040ccbd45a4aa
gaoyx <melo@gaoyuexiang>
2017-03-28 00:08:22 +0800

    add test case for given one tag and fixed it

--- spec/pos/mapTagsToCountedBarcodesSpec.js
+++ spec/pos/mapTagsToCountedBarcodesSpec.js
@@ -3,4 +3,9 @@ describe('map tags to barcodes with count', () => {
   it('should return empty array when given an empty array', () => {
     expect(mapTagsToCountedBarcodes([])).toEqual([]);
   });
+  it('should return countedBarcodes with count is 1 when given tags without repeat barcode', () => {
+    const tags = ['ITEM00001'];
+    const expectCountedBarcode = [{barcode: 'ITEM00001', count: 1}];
+    expect(mapTagsToCountedBarcodes(tags)).toEqual(expectCountedBarcode);
+  });
 });
