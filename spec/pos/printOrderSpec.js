1de1cc4e35b0244edfeb821b929c6ae7e14c7e1d
gaoyx <melo@gaoyuexiang.cn>
2017-03-27 22:43:47 +0800

    fixed test case for printOrder

--- spec/pos/printOrderSpec.js
+++ spec/pos/printOrderSpec.js
@@ -1,4 +1,5 @@
 describe('print order', () => {
+  const printOrder = require('../../lib/pos/printOrder');
   it('should print ticket in console when given an order', function () {
     const order = {
       orderItems: [{
@@ -46,7 +47,7 @@ describe('print order', () => {
 ----------------------
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
 名称：荔枝，数量：1斤，单价：15.00(元)，小计：15.00(元)
-名称：方便面，数量：2袋，单价：4.50(元)，小计：9.00(元)
+名称：泡面，数量：2袋，单价：4.50(元)，小计：9.00(元)
 ----------------------
 总计：36.00(元)
 节省：3.00(元)
